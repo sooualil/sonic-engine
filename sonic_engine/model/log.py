800a9768eba2bce44058cf95e328e3b90304fb6b
souualil <soufiane.oualil.doc20@ensem.ac.ma>
2025-09-05 03:19:37 +0100

    logging model missing

--- sonic_engine/model/log.py
+++ sonic_engine/model/log.py
@@ -6,29 +6,12 @@ import logging
 logging.basicConfig(format="%(name)s [%(levelname)s] %(message)s")
 
 
-class Logger:
-    def __init__(self, config: LogConfig, ctx: str):
-        self.ctx = ctx
-        self.config = config
-        self.level = getattr(logging, config.level)
-        self.l = logging.getLogger(name=ctx)
-        self.l.setLevel(self.level)
+@nested_dataclass
+class LogOptions:
+    "Extension log options"
 
-    def log(self, level: int, msg: str, *args):
-        msg = " ".join((str(arg) for arg in (msg,) + args))
-        self.l.log(level, msg)
+    level: Literal["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"] = "DEBUG"
+    "Log level"
 
-    def debug(self, msg: str, *args):
-        self.log(logging.DEBUG, str(msg), *args)
-
-    def info(self, msg: str, *args):
-        self.log(logging.INFO, str(msg), *args)
-
-    def warning(self, msg: str, *args):
-        self.log(logging.WARNING, str(msg), *args)
-
-    def error(self, msg: str, *args):
-        self.log(logging.ERROR, str(msg), *args)
-
-    def critical(self, msg: str, *args):
-        self.log(logging.CRITICAL, str(msg), *args)
+    dir: str = "./logs"
+    "Path to the logs folder"
