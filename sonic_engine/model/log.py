324da19278bab25f67eb4ffd9e0baf6dfb50b2cd
souualil <soufiane.oualil.doc20@ensem.ac.ma>
2025-09-05 03:33:43 +0100

    restored logger

--- sonic_engine/model/log.py
+++ sonic_engine/model/log.py
@@ -15,3 +15,31 @@ class LogOptions:
 
     dir: str = "./logs"
     "Path to the logs folder"
+
+
+class Logger:
+    def __init__(self, config: LogConfig, ctx: str):
+        self.ctx = ctx
+        self.config = config
+        self.level = getattr(logging, config.level)
+        self.l = logging.getLogger(name=ctx)
+        self.l.setLevel(self.level)
+
+    def log(self, level: int, msg: str, *args):
+        msg = " ".join((str(arg) for arg in (msg,) + args))
+        self.l.log(level, msg)
+
+    def debug(self, msg: str, *args):
+        self.log(logging.DEBUG, str(msg), *args)
+
+    def info(self, msg: str, *args):
+        self.log(logging.INFO, str(msg), *args)
+
+    def warning(self, msg: str, *args):
+        self.log(logging.WARNING, str(msg), *args)
+
+    def error(self, msg: str, *args):
+        self.log(logging.ERROR, str(msg), *args)
+
+    def critical(self, msg: str, *args):
+        self.log(logging.CRITICAL, str(msg), *args)
